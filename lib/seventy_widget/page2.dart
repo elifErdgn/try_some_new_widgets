095d8ebe1da8ea080581bf07b6f51bc8338cd572
elifErdgn <erdgn.elif.37@gmail.com>
2023-03-22 16:30:28 +0300

    ikinci kayit

--- lib/seventy_widget/page2.dart
+++ lib/seventy_widget/page2.dart
@@ -56,7 +56,7 @@ class _Page2State extends State<Page2> {
           border: TableBorder.all(color: Colors.red),
           defaultVerticalAlignment: TableCellVerticalAlignment.middle,
           children: [
-           const TableRow(
+            const TableRow(
               decoration: BoxDecoration(color: Colors.redAccent),
               children: [
                 TableCell(
@@ -114,6 +114,189 @@ class _Page2State extends State<Page2> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget aletButtonuu() {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Close"),
+                ),
+              ],
+              title: Text("Fluter Mapp"),
+              contentPadding: EdgeInsets.all(20),
+              content: Text("Aler dialog içeriği bulunan kısım"),
+            ),
+          );
+        },
+        child: Text("Dialogu aç"),
+      ),
+    );
+  }
+
+  Widget dragTaqrget() {
+    Color caughtColor = Colors.red;
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Draggable(
+            data: Colors.orangeAccent,
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.orangeAccent,
+              child: Center(
+                child: Text("Box"),
+              ),
+            ),
+            onDraggableCanceled: (velocirt, offset) {},
+            feedback: Container(
+              width: 150,
+              height: 150,
+              color: Colors.orangeAccent.withOpacity(0.5),
+              child: const Center(
+                child: Text(
+                  "Box...",
+                  style: TextStyle(
+                    color: Colors.white,
+                    decoration: TextDecoration.none,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          DragTarget(
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return Container(
+                width: 200,
+                height: 200,
+                color: accepted.isEmpty ? caughtColor : Colors.grey.shade200,
+                child: Center(
+                  child: Text("Drag here"),
+                ),
+              );
+            },
+            onAccept: (Color color) {
+              caughtColor = color;
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget slider() {
+    bool _bool = true;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: 100,
+        ),
+        TextButton(
+          onPressed: () {
+            setState(() {
+              _bool = !_bool;
+            });
+          },
+          child: Text("Switch"),
+        ),
+        AnimatedCrossFade(
+            firstChild: Image.asset(
+              "",
+              width: double.infinity,
+            ),
+            secondChild: Image.asset(
+              "",
+              width: double.infinity,
+            ),
+            crossFadeState:
+                _bool ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+            duration: Duration(seconds: 1))
+      ],
+    );
+  }
+}
+
+class silinebilenListWidget extends StatefulWidget {
+  const silinebilenListWidget({Key? key}) : super(key: key);
+
+  @override
+  State<silinebilenListWidget> createState() => _silinebilenListWidgetState();
+}
+
+class _silinebilenListWidgetState extends State<silinebilenListWidget> {
+  List<int> items = List<int>.generate(100, (index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      padding: EdgeInsets.symmetric(vertical: 16),
+      itemBuilder: (BuildContext context, int index) {
+        return Dismissible(
+          background: Container(
+            child: Icon(Icons.delete),
+            color: Colors.red,
+          ),
+          key: ValueKey<int>(items[index]),
+          child: ListTile(
+            title: Text("Item ${items[index]}"),
+          ),
+          onDismissed: (DismissDirection direction) {
+            setState(() {
+              items.removeAt(index);
+            });
+          },
+        );
+      },
+    );
+  }
+}
+
+class dragANDscrollable extends StatefulWidget {
+  const dragANDscrollable({Key? key}) : super(key: key);
+
+  @override
+  State<dragANDscrollable> createState() => _dragANDscrollableState();
+}
+
+class _dragANDscrollableState extends State<dragANDscrollable> {
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      builder: (BuildContext context, ScrollController scrollController) {
+        return Container(
+          color: Colors.orangeAccent,
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text("Item $index"),
+              );
+            },
+            itemCount: 25,
+            controller: scrollController,
+          ),
+        );
+      },
     );
   }
 }
